33f324065072759c4b0f705ad1a6d37284d1dc10
Denis <77487610+Denis11333@users.noreply.github.com>
2022-08-17 09:55:43 +0300

    Update connect-mongo.js

--- config/connect-mongo.js
+++ config/connect-mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const mongoConfig = require('./config-mongo')
 
-mongoose.connect('mongodb://192.168.0.226:27017/digital-department-viti?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false')
+mongoose.connect('***')
     .then(() => {
         console.log('connection to database established');
     }).catch(err => {
