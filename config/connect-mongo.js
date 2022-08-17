732b1e00f6cc35ca73ce1e0be20dcd9c5fa4f30e
Denis <77487610+Denis11333@users.noreply.github.com>
2022-08-17 09:56:44 +0300

    Update connect-mongo.js

--- config/connect-mongo.js
+++ config/connect-mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const mongoConfig = require('./config-mongo')
 
-mongoose.connect('***')
+mongoose.connect('mongodb://localhost:27017/')
     .then(() => {
         console.log('connection to database established');
     }).catch(err => {
